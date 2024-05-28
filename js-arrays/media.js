75e2c72d5dfa714538a0dfa719b64c27b37eeaf4
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-28 12:03:48 -0300

    Create media.js

---
+++ js-arrays/media.js
@@ -0,0 +1,21 @@
+//const nota1 = 10;
+//const nota2 = 6.5;
+//const nota3 = 8;
+//const nota4 = 7.5;
+
+//arrays utilizam os colchetes []
+
+//posiçoes/indices => é contado a partir do 0, exemplo:
+//             0    1   2   3
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+//const.length => calcula a quantidade de constantes no array, sem precisar contar
+
+console.log(media);
+
+console.log("este número esta na posição 0 do array: " + notas[0]);
+console.log("este número está na posição 1 do array: " + notas[1]);
+console.log("este número está na posição 2 do array: " + notas[2]);
+console.log("este número esta na posição 3 do array: " + notas[3]);
+console.log("este número está na posição 4 do array: " + notas[4]);
