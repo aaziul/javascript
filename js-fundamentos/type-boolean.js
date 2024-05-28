854ebe82f100974e9bcf9efd39d6c186c4b14efe
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:40:13 -0300

    Create type-boolean.js

---
+++ js-fundamentos/type-boolean.js
@@ -0,0 +1,22 @@
+// tipo boolean (true or false), para comparações
+
+// true -> verdadeiro
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+// false -> falso
+
+const terceiroNumero = 5;
+const quartoNumero = 10;
+
+console.log(terceiroNumero === quartoNumero);
+
+//comparações com textos
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
