6283c1958cbe07b7ef91253a16ca06fbe47613f4
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:36:50 -0300

    Create conversoes.js

---
+++ js-fundamentos/conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão inplicita, podem trazer bugs, mas compara string com numero e etc. com operações, como soma se torna concatenação
+const numero = 456;
+const numeroString = "456";
+
+//Number() => transforma um caractere em um número
+//String() => transforma um caractere em uma string
+
+console.log(numero + Number(numeroString));
+
+// conversão explicita
