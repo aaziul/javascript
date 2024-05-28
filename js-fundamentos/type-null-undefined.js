0cde74a297c8f1ae4399b56f5fd6737660adf04a
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:40:39 -0300

    Create type-null-undefined.js

---
+++ js-fundamentos/type-null-undefined.js
@@ -0,0 +1,10 @@
+// tipo null e undefined 
+
+let input = null;
+let input2;
+
+// 'null' é um tipo especial, pois pode ser traduzido como “ausência de valor” e pode ser atribuído como valor de uma variável
+console.log(input); 
+
+//undefined se refere ao valor de uma variável que não foi inicializada (ou seja, não foi atribuído nenhum valor a ela). também é o valor retornado por uma função que não tem cláusula return
+console.log(input2); 
