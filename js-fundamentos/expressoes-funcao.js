4deced09b78127937c815051a6ed334904747e25
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:37:16 -0300

    Create expressoes-funcao.js

---
+++ js-fundamentos/expressoes-funcao.js
@@ -0,0 +1,28 @@
+//DECLARAÇÃO DE FUNÇÃO
+
+//function minhaFuncao(parametros) {
+    //bloco de codigo
+//}
+
+//minhaFuncao("param")
+
+//EXPRESSÃO DE FUNÇÃO
+
+//const soma = function(num1, num2){return num1 + num2}
+//console.log(soma(1, 1))
+
+//DIFERENÇA PRINCIPAL ENTRE EXPRESSÃO DE FUNÇÃO E DECLARAÇÃO DE FUNÇÃO: hoisting
+
+console.log(apresentar ())
+
+function apresentar(){
+    return "olá";
+}
+
+//---------------------
+
+console.log(soma(1,1))
+
+const soma = function(num1, num2){return num1 + num2}
+
+// funções e var são listadas no topo do arquivo!!!
