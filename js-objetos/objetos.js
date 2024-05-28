0ccbc9070da4ad89e4c6ed840ffa2ed40e697628
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-28 12:07:46 -0300

    Create objetos.js

---
+++ js-objetos/objetos.js
@@ -0,0 +1,12 @@
+const listaCPFs = [111111, 222222, 3333333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111222333"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: "José",
+    idade: 32,
+}
+
+//um objeto em javascript é uma entidade independente com propriedades e tipos
