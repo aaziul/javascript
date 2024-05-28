670c35d4a42ef72178d3ad62609a8f3b00e3cff8
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:38:55 -0300

    Create template-strings.js

---
+++ js-fundamentos/template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Lu";
+const idade = 2023-2004;
+const cidadeDeNascimento = "Porto Alegre";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
