b33737203988ed872d533063d20b24d9145a54ea
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-28 11:29:56 -0300

    Create atualizando-lista.js

---
+++ js-arrays/atualizando-lista.js
@@ -0,0 +1,8 @@
+//remover Ana e Caio e adicionar Rodrigo
+
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "Rodrigo");
+//nomes.push("Rodrigo");
+
+console.log(nomes);
