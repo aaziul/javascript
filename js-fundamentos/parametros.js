8b8d778496f1400f25326e29d9a5092c6076a24e
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:38:21 -0300

    Create parametros.js

---
+++ js-fundamentos/parametros.js
@@ -0,0 +1,23 @@
+// parametros de função
+
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+console.log(soma(200,300));
+
+//parametros => possuem ordem, ex: var num1 recebe valor 200, var num2 recebe valor 300
+
+//argumentos =>
+
+//function nomeIdade(nome, idade){
+    //return `meu nome é ${nome} e minha idade é ${idade}`;
+//}
+
+//console.log(nomeIdade(Luiza, 18))
+
+function multiplica(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiplica(soma(2,3), soma(5,6)))
