c588f9b8d1f6b72b2e08e9b77c3429ed7e1eee17
luiza <97320223+aaziul@users.noreply.github.com>
2024-05-27 22:40:59 -0300

    Create type-number.js

---
+++ js-fundamentos/type-number.js
@@ -0,0 +1,19 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuane
+
+var numeroPontoFlutuante = 3.3;
+var numeroFlutuanteSemZero = .5;
+
+var novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
